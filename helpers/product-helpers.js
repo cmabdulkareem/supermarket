63a7ee4a8d25777c657ded91a3c4e59d4b57eea0
cmabdulkareem <cmabdulkareem@gmail.com>
2024-03-22 11:21:04 +0530

    added db connection to user page

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -4,7 +4,7 @@ module.exports={
     addProduct:(product,callback)=>{
         console.log(product)
         db.get().collection('product').insertOne(product).then((data)=>{
-            console.log(data.insertedId)
+            console.log(data.insertedId)  
             callback(data.insertedId)
         })
     },
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var productHelper= require("../helpers/product-helpers")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  let fruits = [    //let fruits = [{},{},{},{}]
-    {
-      name:"Apple",
-      description:"kashmiri royal apple from Kashmir india",
-      price : 200,
-      image : "https://shorturl.at/fgmoE"
-    },
-    {
-        name:"Orange",
-        description:"Nagpur orange from Nagpur india",
-        price : 75,
-        image : "https://shorturl.at/hCLTU"
-    },
-    {
-        name:"Kiwi",
-        description:"Australian rich kiwi",
-        price : 300,
-        image : "https://shorturl.at/nAJVX"
-    },
-    {
-        name:"Banana",
-        description:"Banana Kadali from Kerala",
-        price : 40,
-        image : "https://shorturl.at/tyHQ7"
-    },
-  ]
+  productHelper.getAllProducts((err, fruits) => {
+    if(err) {
+      console.log(err);
+    } else {
+        console.log(fruits._id);
+      res.render('index', { fruits, admin: false });
+    }
+  });
 
-
-  res.render('index', { fruits, admin:false });
 });
 
 module.exports = router;
